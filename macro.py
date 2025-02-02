bf5c260ea41a22986db81e9f52089626915ed33d
xmichal88x <98843330+xmichal88x@users.noreply.github.com>
2025-02-02 15:39:58 +0100

    Update macro.py

--- macro.py
+++ macro.py
@@ -1,32 +1,11 @@
-import time
-from simcnc_api import d, IOPortDir, DIOPinVal  # Import API SimCNC
+from ___CONF import * 
+import time   
+import sys
 
-# Konfiguracja wejść i wyjść
+timezone = time.localtime() 
 
-# Sekcja wyjść (sterowanie)
-OUT_CURTAIN_UP = 9             # Wyjście do podnoszenia szczotki
-OUT_CURTAIN_DOWN = 8           # Wyjście do opuszczania szczotki
-OUT_AGGREGATE_UP = 7           # Wyjście do podnoszenia agregatu
-OUT_CLEANCONE = 12             # Czyszczenie stożka
-OUT_AGGREGATE_DOWN = 6         # Wyjście do opuszczania agregatu
-OUT_TOOL_CHANGE_POS = 2        # Wyjście do aktywacji pozycji wymiany narzędzia
-OUT_COLLECT_OPEN = 10          # Wyjście do otwierania uchwytu narzędzia
-OUT_COLLECT_CLOSE = 11         # Wyjście do zamykania uchwytu narzędzia
-OUT_MAGAZINE_OPEN = 4          # Wyjście do otwierania magazynu
-OUT_MAGAZINE_CLOSE = 5         # Wyjście do zamykania magazynu
+mode = "debug" # normal or debug (for more info output)
 
-# Sekcja wejść (czujniki)
-IN_COLLET_OPENED = 13          # Czujnik otwarcia uchwytu narzędzia
-IN_TOOL_INSIDE = 12            # Czujnik obecności narzędzia w uchwycie
-IN_SzczotkaUp = 6              # Czujnik górnej pozycji szczotki
-IN_SzczotkaDown = 5            # Czujnik dolnej pozycji szczotki
-IN_OslonaPionOpen = 7          # Czujnik otwarcia osłony pionowej
-IN_OslonaPionClose = 8         # Czujnik zamknięcia osłony pionowej
-IN_OslonaPozOpen = 9           # Czujnik otwarcia osłony poziomej
-IN_OslonaPozClose = 11         # Czujnik zamknięcia osłony poziomej
-IN_AGREGAT1_UP = 3             # Czujnik górnej pozycji agregatu A1
-IN_AGREGAT1_Down = 4           # Czujnik dolnej pozycji agregatu A1
-IN_NarzedzieWMagazynie = 14    # Czujnik obecności narzędzia w magazynie
 
 # Główna funkcja programu
 def main():
