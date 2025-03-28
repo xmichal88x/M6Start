e2e85b6b5c8ce14a251fe58f57c18656e1967e91
xmichal88x <98843330+xmichal88x@users.noreply.github.com>
2025-03-28 12:20:37 +0100

    Update tool_probe.py

--- tool_probe.py
+++ tool_probe.py
@@ -1,18 +1,178 @@
-
-
+      
 from ___CONF import *
-from ___FUNCTION import *
 import sys
 import time
 import json
 
+timezone = time.localtime() 
+
 JSON_FILE = "narzedzia.json"
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# FUNCTION to throw message in py status line and optionally end program 
+# Args: message(string), action(boolean)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def throwMessage(message, action):
+
+    ttime = time.strftime("%H:%M:%S", timezone)
+    print("\n"  + ttime + " - " + message)
+
+    if message == True: 
+        
+        msg.info("\n"  + ttime + " - " + message)
+
+    if action == "exit":
+        sys.exit(0)
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# FUNCTION to GET status of IO pin
+# Args: pin_in(int)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def get_digital_input(pin_tuple):
+    """Odczytuje stan wejścia cyfrowego z odpowiedniego modułu."""
+    module_id, pin, module_type = pin_tuple
+
+    # Wybór odpowiedniego modułu
+    if module_type == "IP":
+        csmio = d.getModule(ModuleType.IP, module_id)
+    elif module_type == "IO":
+        csmio = d.getModule(ModuleType.IO, module_id)
+    else:
+        print(f"Błąd: Nieznany typ modułu {module_type}")
+        return None
+
+    if pin is None:
+        print("Błąd: Pin nie został podany.")
+        return None
+
+    value = csmio.getDigitalIO(IOPortDir.InputPort, pin) == DIOPinVal.PinSet
+    # print(f"Odczytano wejście: module_id={module_id}, pin={pin}, wartość={value}")
+    return value
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# FUNCTION to SET status of IO pin
+# Args: pin_out(int), state(boolean)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def set_digital_output(pin_tuple, state):
+    """Ustawia stan wyjścia cyfrowego w odpowiednim module."""
+    module_id, pin, module_type = pin_tuple  # Rozpakowanie wartości
+    state2 = DIOPinVal.PinSet if state else DIOPinVal.PinReset
+
+    # Wybór odpowiedniego modułu
+    if module_type == "IP":
+        csmio = d.getModule(ModuleType.IP, module_id)
+    elif module_type == "IO":
+        csmio = d.getModule(ModuleType.IO, module_id)
+    else:
+        print(f"Błąd: Nieznany typ modułu {module_type}")
+        return None
+
+    if pin is None:
+        print("Błąd: Pin nie został podany.")
+        return None
+    
+    try:
+        csmio.setDigitalIO(pin, state2)
+    except NameError as e:
+        print(f"Błąd: Digital Output został błędnie zdefiniowany. Szczegóły: {e}")
+
+#-----------------------------------------------------------
+# Lista programów
+#-----------------------------------------------------------
+
+def check_axes_referenced():
+    axis_to_check = [Axis.X, Axis.Y, Axis.Z]
+    not_referenced_axes = []  # Lista na niezreferowane osie
+
+    for axis in axis_to_check:
+        if not d.isAxisReferenced(axis):
+            not_referenced_axes.append(axis)  # Dodajemy niezreferowaną oś
+
+    # Jeśli są niezreferowane osie, zgłoś błąd
+    if not_referenced_axes:
+        msg_axes_referenced = f"🔴 Osi(e) {', '.join([str(axis) for axis in not_referenced_axes])} nie są zbazowane! Uruchom proces bazowania."
+        throwMessage(msg_axes_referenced, "exit")
+
+def curtain_up():
+    """
+    Podnosi szczotkę.
+    - Sprawdza czujnik pozycji górnej szczotki.
+    """
+    print("Rozpoczynam podnoszenie szczotki...")
+    set_digital_output(OUT_CURTAIN_UP, True)
+    time.sleep(0.25)
+    set_digital_output(OUT_CURTAIN_UP, False)
+
+    start_time = time.time()
+    while not get_digital_input(IN_CURTAIN_UP):
+        if time.time() - start_time > 5:
+            print("Błąd: Szczotka nie osiągnęła pozycji górnej.")
+            return False
+        time.sleep(0.1)
+    print("Szczotka podniesiona.")
+    return True
+
+def curtain_down():
+    """
+    Opuszcza szczotkę.
+    - Sprawdza czujnik pozycji dolnej szczotki.
+    """
+    print("Rozpoczynam opuszczanie szczotki...")
+    set_digital_output(OUT_CURTAIN_DOWN, True)
+    time.sleep(0.25)
+    set_digital_output(OUT_CURTAIN_DOWN, False)
+
+    start_time = time.time()
+    while not get_digital_input(IN_CURTAIN_DOWN):
+        if time.time() - start_time > 5:
+            print("Błąd: Szczotka nie osiągnęła pozycji dolnej.")
+            return False
+        time.sleep(0.1)
+    print("Szczotka opuszczona.")
+    return True
+
+def activate_tool_change_position():
+    """
+    Aktywuje pozycję wymiany narzędzia.
+    """
+    print("Aktywuję pozycję wymiany narzędzia...")
+    set_digital_output(OUT_TOOL_CHANGE_POS, True)
+    time.sleep(0.25)
+    print("Pozycja wymiany aktywowana.")
+
+def deactivate_tool_change_position():
+    """
+    Dezaktywuje pozycję wymiany narzędzia.
+    """
+    print("Dezaktywuję pozycję wymiany narzędzia...")
+    set_digital_output(OUT_TOOL_CHANGE_POS, False)
+    time.sleep(0.25)
+    print("Pozycja wymiany dezaktywowana.")
+
+
+
 
 #############################################
 # Macro START - Uruchomienie procedury pomiaru długości narzędzia
 #############################################
 
-ustaw_stan_procesu("Pomiar")
+#-----------------------------------------------------------
+# Perform pre-checks
+#-----------------------------------------------------------
+
+# exit if axes not referenced
+check_axes_referenced()
+
+# Aktywuj pozycję wymiany
+activate_tool_change_position()
+
+# ignore softlimits
+d.ignoreAllSoftLimits(True)
+    
+#ustaw_stan_procesu("Pomiar")
 
 # Wyłącz wrzeciono
 d.setSpindleState(SpindleState.OFF)
@@ -78,6 +238,17 @@ d.setToolLength(toolNr, toolOffset)
 machine_pos[Z] = Z_SAFE
 d.moveToPosition(CoordMode.Machine, machine_pos, FEED_PROBE_MOVE)
 
+# przejedź do pozycji poza obaszarem wymiany narzędzia
+machine_pos[Y] = 1550
+d.moveToPosition(CoordMode.Machine, machine_pos, feed_atc_xy)
+
+# Dezaktywuje pozycję wymiany
+deactivate_tool_change_position()
+
+# Przywrócenie softlimitów
+d.ignoreAllSoftLimits(False)
+print("Softlimity przywrócone.")
+
 # Zakończenie programu
 print(f"Tool({toolNr}) offset set to: {toolOffset:.4f}")
-ustaw_stan_procesu(None)
+#ustaw_stan_procesu(None)
